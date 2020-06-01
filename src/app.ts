25c835c4b61a4dc89478cb24091390d13c5aa075
Arafat Husayn <hello@arafat.dev>
2020-06-01 20:19:46 +0600

    format code

--- src/app.ts
+++ src/app.ts
@@ -1,101 +1,101 @@
-import fetch, { Response } from "node-fetch"
-import { JSDOM } from "jsdom"
+import fetch, { Response } from "node-fetch";
+import { JSDOM } from "jsdom";
 
-type FinalJsonDataType = { title: string; year: string }[]
+type FinalJsonDataType = { title: string; year: string }[];
 
-console.log(new Date().toString())
+console.log(new Date().toString());
 
-console.log("== Starting the scrapper ==")
+console.log("== Starting the scrapper ==");
 
 const main = async (): Promise<void> => {
-  let finalData: FinalJsonDataType = []
+  let finalData: FinalJsonDataType = [];
 
-  const response: Response = await fetch("https://www.imdb.com/feature/genre/")
+  const response: Response = await fetch("https://www.imdb.com/feature/genre/");
 
-  const body: string = await response.text()
+  const body: string = await response.text();
 
   const matches: RegExpMatchArray | null = body.match(
     /\/search\/title\?genres=\w+&title_type=feature&explore=genres/g
-  )
+  );
 
   const urls: string[] | null =
     matches &&
-    matches.map((m) => `https://www.imdb.com${m}&sort=alpha,asc&view=simple`)
+    matches.map((m) => `https://www.imdb.com${m}&sort=alpha,asc&view=simple`);
 
-  console.log("Genres: ", urls)
+  console.log("Genres: ", urls);
 
   if (!urls) {
-    console.log("No Genres to start!")
-    process.exit(1)
+    console.log("No Genres to start!");
+    process.exit(1);
   }
 
   for (const url of urls) {
-    let response: Response
-    let body: string
+    let response: Response;
+    let body: string;
 
-    console.log("Scrapping:", url)
+    console.log("Scrapping:", url);
 
-    response = await fetch(url)
+    response = await fetch(url);
 
-    body = await response.text()
+    body = await response.text();
 
     let titlesCountMatch: RegExpMatchArray | null = body.match(
       /(([0-9]|,)+) titles/
-    )
+    );
 
     const titlesCount =
-      (titlesCountMatch && parseInt(titlesCountMatch[1].replace(",", ""))) || 0
+      (titlesCountMatch && parseInt(titlesCountMatch[1].replace(",", ""))) || 0;
 
-    console.log("Total titles:", titlesCount)
+    console.log("Total titles:", titlesCount);
 
-    let nextPageLinkMatch: RegExpMatchArray | null
+    let nextPageLinkMatch: RegExpMatchArray | null;
 
-    let pageCounter: number = 1
+    let pageCounter: number = 1;
 
     while (true) {
       const {
         window: { document },
-      } = new JSDOM(body)
+      } = new JSDOM(body);
 
       const titles: (string | null)[] = Array.from(
         document.querySelectorAll(".col-title a")
-      ).map((el) => el.textContent)
+      ).map((el) => el.textContent);
 
       const years: (string | null)[] = Array.from(
         document.querySelectorAll(".col-title .lister-item-year")
       ).map(
         (el) =>
           el.textContent && el.textContent.replace("(", "").replace(")", "")
-      )
+      );
 
       const data: FinalJsonDataType = titles.map((t, i) => ({
         title: t || "",
         year: years[i] || "",
-      }))
+      }));
 
-      finalData = finalData.concat(data)
+      finalData = finalData.concat(data);
 
-      console.log("final data length:", finalData.length)
+      console.log("final data length:", finalData.length);
 
-      nextPageLinkMatch = body.match(/a href="((.+)adv_nxt)/)
+      nextPageLinkMatch = body.match(/a href="((.+)adv_nxt)/);
 
       if (!nextPageLinkMatch) {
-        console.log("> No next page. Finishing this genre...")
-        break
+        console.log("> No next page. Finishing this genre...");
+        break;
       }
 
-      const nextPageLink: string = `https://www.imdb.com${nextPageLinkMatch[1]}`
+      const nextPageLink: string = `https://www.imdb.com${nextPageLinkMatch[1]}`;
 
-      console.log(`Next page #${++pageCounter}: ${nextPageLink}`)
+      console.log(`Next page #${++pageCounter}: ${nextPageLink}`);
 
-      response = await fetch(nextPageLink)
+      response = await fetch(nextPageLink);
 
-      body = await response.text()
+      body = await response.text();
     }
   }
-}
+};
 
 main().catch((e: Error): void => {
-  console.error(e)
-  process.exit(1)
-})
+  console.error(e);
+  process.exit(1);
+});
