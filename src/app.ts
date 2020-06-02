b61f59778058e6fd77062737643c2e60f841c789
Arafat Husayn <hello@arafat.dev>
2020-06-02 14:38:33 +0600

    store final data as csv and json

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
+import fs from "fs";
+import path from "path";
 import fetch, { Response } from "node-fetch";
 import { JSDOM } from "jsdom";
+import { Transform } from "json2csv";
 
-type FinalJsonDataType = { title: string; year: string }[];
+type Movie = {
+  title: string;
+  year: string;
+  genres: string[];
+};
+
+type FinalJsonDataType = Movie[];
+
+const movieMatches = (movie: Movie, m: Movie): boolean =>
+  movie.title === m.title && movie.year === m.year;
 
 console.log(new Date().toString());
 
@@ -15,7 +27,7 @@ const main = async (): Promise<void> => {
   const body: string = await response.text();
 
   const matches: RegExpMatchArray | null = body.match(
-    /\/search\/title\?genres=\w+&title_type=feature&explore=genres/g
+    /\/search\/title\?genres=\w+&title_type=feature&explore=genres/g,
   );
 
   const urls: string[] | null =
@@ -35,12 +47,23 @@ const main = async (): Promise<void> => {
 
     console.log("Scrapping:", url);
 
+    const genreNullable: string | null = new URL(url).searchParams.get(
+      "genres",
+    );
+
+    if (!genreNullable) {
+      console.error("No genre in the URL!");
+      process.exit(1);
+    }
+
+    const genres: string[] = [genreNullable];
+
     response = await fetch(url);
 
     body = await response.text();
 
     let titlesCountMatch: RegExpMatchArray | null = body.match(
-      /(([0-9]|,)+) titles/
+      /(([0-9]|,)+) titles/,
     );
 
     const titlesCount =
@@ -58,20 +81,42 @@ const main = async (): Promise<void> => {
       } = new JSDOM(body);
 
       const titles: (string | null)[] = Array.from(
-        document.querySelectorAll(".col-title a")
+        document.querySelectorAll(".col-title a"),
       ).map((el) => el.textContent);
 
       const years: (string | null)[] = Array.from(
-        document.querySelectorAll(".col-title .lister-item-year")
+        document.querySelectorAll(".col-title .lister-item-year"),
       ).map(
         (el) =>
-          el.textContent && el.textContent.replace("(", "").replace(")", "")
+          el.textContent && el.textContent.replace("(", "").replace(")", ""),
       );
 
-      const data: FinalJsonDataType = titles.map((t, i) => ({
+      let data: FinalJsonDataType = titles.map((t, i) => ({
         title: t || "",
         year: years[i] || "",
+        genres,
       }));
+
+      for (const movie of data) {
+        const movieInFinalDataIndex: number = finalData.findIndex((m) =>
+          movieMatches(movie, m),
+        );
+
+        if (movieInFinalDataIndex === -1) {
+          continue;
+        }
+
+        const oldGenres = finalData[movieInFinalDataIndex].genres;
+
+        finalData[movieInFinalDataIndex] = {
+          ...finalData[movieInFinalDataIndex],
+          genres: oldGenres.concat(movie.genres),
+        };
+      }
+
+      data = data.filter(
+        (movie) => !finalData.find((m) => movieMatches(movie, m)),
+      );
 
       finalData = finalData.concat(data);
 
@@ -92,6 +137,64 @@ const main = async (): Promise<void> => {
 
       body = await response.text();
     }
+  }
+
+  console.log("Saving Final Data...");
+
+  const filePathPrefix: string = `${path.resolve(
+    path.join(__dirname, ".."),
+  )}/output/${new Date().getFullYear()}_${
+    new Date().getMonth() + 1
+  }_${new Date().getDate()}_${Date.now()}`;
+
+  const jsonFilePath: string = `${filePathPrefix}.json`;
+
+  const csvFilePath: string = `${filePathPrefix}.csv`;
+
+  fs.writeFileSync(jsonFilePath, JSON.stringify(finalData), {
+    encoding: "utf-8",
+  });
+
+  const input: fs.ReadStream = fs.createReadStream(jsonFilePath, {
+    encoding: "utf8",
+  });
+
+  const output: fs.WriteStream = fs.createWriteStream(csvFilePath, {
+    encoding: "utf8",
+  });
+
+  const json2csv: Transform<Movie> = new Transform(
+    {
+      fields: [
+        "title",
+        "year",
+        {
+          label: "genres",
+          value: (row: Movie) => row.genres.join(","),
+        },
+      ],
+    },
+    {
+      encoding: "utf-8",
+    },
+  );
+
+  input.pipe(json2csv).pipe(output);
+
+  const promise: Promise<boolean> = new Promise((resolve, reject) => {
+    json2csv
+      .on("end", () => {
+        console.log("CSV write stream ended.");
+        resolve(true);
+      })
+      .on("error", (err) => {
+        console.error(err);
+        reject();
+      });
+  });
+
+  if ((await promise) == true) {
+    console.log("CSV file saved successfully.");
   }
 };
 
